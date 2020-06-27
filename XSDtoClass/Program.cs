569024886ca80cb369e20ec04ae19be5551a3e1f
dev-crisloli <67495415+dev-crisloli@users.noreply.github.com>
2020-06-27 00:16:49 -0500

    Proyecto Correcto
    Mas informacion en el video de Youtube:
    
    https://www.youtube.com/watch?v=SNpp_OhSY68

---
+++ XSDtoClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace XSDtoClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InvoiceType invoiceType = new InvoiceType();
+
+            UBLVersionIDType uBLVersionIDType = new UBLVersionIDType();
+            uBLVersionIDType.Value = "2.1";
+            invoiceType.UBLVersionID = uBLVersionIDType;
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(InvoiceType));
+            var oStringWriter = new StringWriter();
+            xmlSerializer.Serialize(XmlWriter.Create(oStringWriter), invoiceType);
+            string stringXML = oStringWriter.ToString();
+            System.IO.File.WriteAllText("XML_Sunat.xml",stringXML);
+
+        }
+    }
+}
--- XSDtoClass/Properties/AssemblyInfo.cs
+++ XSDtoClass/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("Prueba_Clase_XSDtoClass")]
+[assembly: AssemblyTitle("XSDtoClass")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Prueba_Clase_XSDtoClass")]
+[assembly: AssemblyProduct("XSDtoClass")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("52e0e6f7-71aa-4463-b5e0-b3a50db3e9e5")]
+[assembly: Guid("ce1295b0-69fd-4744-97a3-606ceb2f889e")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
