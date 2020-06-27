b2d11165eb749bc0f8b18e20c979211f1a3db47b
dev-crisloli <67495415+dev-crisloli@users.noreply.github.com>
2020-06-27 00:10:37 -0500

    Proyecto Inicial Curso
    Video de Referencia
    
    https://www.youtube.com/watch?v=SNpp_OhSY68

---
+++ Prueba_Clase_XSDtoClass/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Prueba_Clase_XSDtoClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nuevo Documento
+            InvoiceType oInvoiceType = new InvoiceType();
+            UBLVersionIDType oUBLVersionIDType = new UBLVersionIDType();
+            oUBLVersionIDType.Value = "2.1";
+
+            oInvoiceType.UBLVersionID = oUBLVersionIDType;
+
+            
+            //Arreglo TaxAmountype
+            TaxAmountType taxAmountType = new TaxAmountType();
+            taxAmountType.Value = 10;
+            taxAmountType.currencyCodeListVersionID = "100"; //Atributos
+
+            //Arreglo de taxSubTotalType
+            TaxSubtotalType taxSubtotalType_igv = new TaxSubtotalType();
+            taxSubtotalType_igv.TaxAmount = taxAmountType;
+            TaxSubtotalType[] taxSubtotalType = { taxSubtotalType_igv };
+
+            //Arreglo TaxTotalType
+            TaxTotalType TaxTotalType_1 = new TaxTotalType();
+            TaxTotalType_1.TaxSubtotal = taxSubtotalType;
+            TaxTotalType[] taxTotalType = { TaxTotalType_1 };
+ 
+            oInvoiceType.TaxTotal = taxTotalType;
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(InvoiceType));
+            var swf = new StringWriter();
+            XmlSerializerNamespaces xmlSerializerNamespaces = new XmlSerializerNamespaces();
+            xmlSerializerNamespaces.Add("cbc","urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2");
+            xmlSerializer.Serialize(XmlWriter.Create(swf),oInvoiceType, xmlSerializerNamespaces);
+            string sXML = swf.ToString();
+            System.IO.File.WriteAllText("prueba.xml", sXML);
+            
+        }
+    }
+}
